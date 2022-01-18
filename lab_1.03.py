f57f8a77426462bbd512b1f9f23f2356ceb5b02c
Jesse <jchauser24@students.bmchs.org>
2022-01-18 09:41:53 -0600

    new lab

---
+++ lab_1.03.py
@@ -0,0 +1,39 @@
+'''
+Lab 1.03 - Printing & Variables
+
+Part 1 - Printing Practice
+Practice typing out some statements in the editor part of the IDE, then hit "Run" at the top of the screen:
+**Remember you must exit() python interpreter before running a script!!!
+
+    Expression                                      Expected Output                               Did anything unexpected happen?
+a)  print("1")                                          1                                                   no
+b)  print(1)                                               1                                                no
+c)  print(1 + 2)                                            3                                               no
+d)  print("1" + "2")                                        12                                              no
+e)  print("this" + " " + "is" + " " +                       ran the code                                    no
+    "a" + " " +
+    "sentence" + ".")
+
+Part 2 - Variables Practice
+1. In your Console
+Type and run the following:
+    animal = "dogs"
+    print(animal + " are really cool.")                     printed it                                         no
+In your Notebook
+Respond to the following:
+    1. What happens? it printed the code
+    2. How would you make the program print out "cats are really cool" instead? animal = "cats" print ("animal" + " are really cool")
+
+2. In your Console
+Type and run the following code:
+print(dogs + " are cool.")
+In your Notebook
+Respond to the following questions:
+    1. What output does this produce?error 
+    2. Why does this happen? dog does not have " around it 
+'''
+print("1")
+print(1)
+print(1 + 2)
+print("1" + "2")
+print("this" + " " + "is" + " " + "a" + " " + "sentence" + ".")
